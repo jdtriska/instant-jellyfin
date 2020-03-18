2cd6d16ab564fb2e145c9222dadfdae8787938b2
Joey Triska <jdtriska@gmail.com>
2020-03-17 23:24:31 -0400

    Completed variables file

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_REGION" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
+
+variable "EBS_VOLUME_SIZE" {
+  type = string
+}
+
+variable "EC2_INSTANCE_TYPE" {
+  type = string
+}
+
+variable "ENVIRONMENT" {
+  type = string
+}
+
+variable "HOSTED_ZONE_ID" {
+  type = string
+}
