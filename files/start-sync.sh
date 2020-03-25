ed9dd84cb51b860e44cee99e2468ac9cab605778
dougkirkley <doug.kirkley@gmail.com>
2020-03-25 16:55:14 -0400

    Update start-sync.sh

--- files/start-sync.sh
+++ files/start-sync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -e "$(crontab -l 2>/dev/null | grep -v jellyfin-s3-sync)\n*/5 * * * * /usr/bin/flock -x /var/log/jellyfin-s3-sync /bin/bash ~/jellyfin/scripts/s3sync.sh #jellyfin-s3-sync" | crontab -
+echo -e "$(crontab -l 2>/dev/null | grep -v jellyfin-s3-sync)\n*/5 * * * * /usr/bin/flock /bin/bash ~/jellyfin/scripts/s3sync.sh #jellyfin-s3-sync" | crontab -
